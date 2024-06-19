7c31a22f6ad9a4f2b3b645012b11d90ecbe9e783
Roshni863 <roshninikale1997@gmail.com>
2024-06-19 15:41:54 +0530

    Update Roshni_Git_project.java

--- Roshni_GitHub/src/com/demo/Roshni_Git_project.java
+++ Roshni_GitHub/src/com/demo/Roshni_Git_project.java
@@ -1,8 +1,17 @@
 package com.demo;
 
+
 public class Roshni_Git_project {
 
+	public void m1()
+	{
+		System.out.println("Roshni")
+			}
+
 	public static void main(String[] args) {
+
+		Roshni_Git_project r=new Roshni_Git_project();
+			r.m1();
 		
 		System.out.println("Hello world");
 	}
